86e889f7ad9669303ba1ed54895fd64e211ca2bc
Angel Rosario <arosario.us@gmail.com>
2024-01-09 12:42:46 +0900

    streaming and cards

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />; //displays a simplified version of UI while the page content loads
+}
